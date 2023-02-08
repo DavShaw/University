9452c87a80d8ef3be2c37f8c920ec99a02067f85
David Torres <119999924+DavShaw@users.noreply.github.com>
2023-02-08 17:00:12 -0500

    Creating Func N1

---
+++ 11_02_23/Functions.py
@@ -0,0 +1,12 @@
+def Act1():
+    user_name = input("¿Cuál es tu nombre?: ")
+    user_age = input("¿Cuál es tu edad?: ")
+    print(f"""
+===============================
+¡Hola! Hemos recolectado tu info
+
+Te llamas: {user_name}
+Tu edad es: {user_age}
+===============================
+    """)
+    return None
