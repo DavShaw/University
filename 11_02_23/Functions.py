ae6d987cb3532c3f29d700052b3c31dd122742ce
David Torres <119999924+DavShaw@users.noreply.github.com>
2023-02-08 17:03:24 -0500

    Creating func Act2

--- 11_02_23/Functions.py
+++ 11_02_23/Functions.py
@@ -1,3 +1,5 @@
+from cmath import pi 
+
 def Act1():
     user_name = input("¿Cuál es tu nombre?: ")
     user_age = input("¿Cuál es tu edad?: ")
@@ -10,3 +12,17 @@ Tu edad es: {user_age}
 ===============================
     """)
     return None
+
+def Act2():
+    radio = input("Ingresa el rado del circulo: ")
+    radio = float(radio)
+    area = pi*(radio**2)
+    print(f"""
+===============================
+¡Hey! El área del circulo en
+base al radio es:
+
+{area}
+===============================
+    """)
+
