50c8f625bbd9cd33723cbe14a53c1b922839fc16
David Torres <119999924+DavShaw@users.noreply.github.com>
2023-02-10 09:22:35 -0500

    Idk what I did

--- 10-02-23/Employee.py
+++ 10-02-23/Employee.py
@@ -1,14 +1,16 @@
-from datetime import datetime
+from datetime import datetime, date
 
 class Employee():
 
-    def __init__(self, name: str = "No definido", dni: str = "000", birthday: str = "??/??/???", join_date: str = "??/??/???", salary: float = 0):
+    def __init__(self, name: str, dni: str, birthday: date, join_date: date, salary: float):
         self.name: str = name
         self.dni: str = dni
-        self.birthday: str = birthday
-        self.join_date: str = join_date
+        self.birthday: date = birthday
+        self.join_date: date = join_date
         self.salary: float = salary
+
+
 
     def Antiquity(self) -> float:
         join_date_diff = datetime.date(datetime.now()) - self.join_date
-        return((join_date_diff.day + join_date_diff.seconds)/86400) / 365
+        return round(((join_date_diff.days + join_date_diff.seconds / 86400) / 365), 1)
--- 10-02-23/Executer.py
+++ 10-02-23/Executer.py
@@ -4,10 +4,10 @@ import datetime
 
 if __name__ == __name__:
 
-    David = Employee("David","1067592686",datetime.date(2004,08,29),datetime.datetime(2021,01,21), 98000)
-    Mateo = Employee("Mateo","1087598240",datetime.date(2001,12,01),datetime.datetime(2020,05,11), 68000)
+    David = Employee("David","1067592686",datetime.date(2004,8,29),datetime.date(2021,1,21), 98000)
+    Mateo = Employee("Mateo","1087598240",datetime.date(2001,12,1),datetime.date(2020,5,11), 68000)
 
     print(f"""
-    David lleva {David.Antiquity()} en la empresa
-    Mateo lleva {Mateo.Antiquity()} en la empresa
+    David lleva {David.Antiquity()} años en la empresa
+    Mateo lleva {Mateo.Antiquity()} años en la empresa
     """)
