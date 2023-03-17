6da9752b529da5342ef89d2d0327c03f4f33727c
David Torres <119999924+DavShaw@users.noreply.github.com>
2023-03-17 01:06:05 -0500

    Descomponiendo requisitos

---
+++ 17-03-23.py
@@ -0,0 +1,45 @@
+#Implementación de clases y métodos del análisis del problema con Black Jack
+
+class BlackJack():
+
+    def detectar_limite():
+        pass
+
+    def doblar_apuesta():
+        pass
+
+    def restar_fichas():
+        pass
+
+    def seguir_jugando() -> bool:
+        pass
+
+class Jugador():
+
+    def jugada():
+        pass
+    
+    def pedir_carta():
+        pass
+
+class Mano():
+
+    def calcular_mano():
+        pass
+
+    def comparar_mano(jugador,casa):
+        pass
+
+class Carta():
+    
+    def destapar_carta(jugador):
+        pass
+
+
+class Baraja():
+    
+    def repartir_carta():
+        pass
+
+class Casa():
+    pass
